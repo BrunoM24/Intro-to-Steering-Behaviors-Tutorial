2c36c72650b8494e14c008375b326f98a3c66330
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-18 19:49:42 +0000

    Target Scene - Created the Target Scene with the Basic Implementation

---
+++ src/UI/Target.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	visible = false
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("click"):
+		animation_player.play("fade_in")
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("click"):
+		global_position = get_global_mouse_position()
+
+
+func _on_body_entered(body: Node) -> void:
+	animation_player.play("fade_out")
