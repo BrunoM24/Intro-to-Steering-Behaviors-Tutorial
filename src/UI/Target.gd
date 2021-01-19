d40543004e2496583df411fd479d490e504aa7fb
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-19 22:08:56 +0000

    Target Scene - Changed the logic to appear on action_pressed instead of on action_released

--- src/UI/Target.gd
+++ src/UI/Target.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("click"):
+	if event.is_action_pressed("click"):
 		animation_player.play("fade_in")
 
 
